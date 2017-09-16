886a47adb0a511103a8134be5bd7e502e66c2bd9
Koogle.Zhang <koogle.zhang@gmail.com>
2017-09-16 11:18:31 +0800

    钱包添加链接到行情

--- app/controllers/trade/dashboard_controller.rb
+++ app/controllers/trade/dashboard_controller.rb
@@ -6,7 +6,7 @@ class Trade::DashboardController < Trade::BaseController
     end_time = params[:end] || Date.current.to_s
     @block = Block.find_by_english(block)
     tickers = @block.tickers.where("that_date >= ? AND that_date <= ?",sta_time,end_time)
-    tickers = @block.tickers.last(48) if tickers.count == 0
+    tickers = @block.tickers.last(48) if tickers.count < 10
     @date_array = tickers.map {|x| x.created_at.strftime('%H:%M')}
     @value_array = tickers.map {|x| x.last_price}
     @ma5_array = tickers.map {|x| x.ma5_price}
